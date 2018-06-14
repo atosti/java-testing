8071ce20ecd6a28cd8f59580b196078e229a38aa
atosti <antonio_tosti@spe.sony.com>
2018-06-13 17:49:50 -0700

    Added a private method to be tested with reflection

--- src/main/java/com/company/app/service/MyService.java
+++ src/main/java/com/company/app/service/MyService.java
@@ -25,4 +25,9 @@ public class MyService{
         }
         return false;
     }
+    
+    //Typically untestable private method
+    private Long addSecretly(Long a, Long b){
+        return a + b;
+    }
 }
