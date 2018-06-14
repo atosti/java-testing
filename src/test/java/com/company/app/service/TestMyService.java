0e546a5cd4d7ef31bc7786683e2f4fc1ef08bb7e
atosti <antonio_tosti@spe.sony.com>
2018-06-14 10:46:49 -0700

    Added some comments outlining each test, and added a test that uses matchers

--- src/test/java/com/company/app/service/TestMyService.java
+++ src/test/java/com/company/app/service/TestMyService.java
@@ -18,17 +18,19 @@ import com.company.app.EmployeeRepository;
  * Unit test for simple MyService.
  */
 public class TestMyService{
-    //Follows naming convention of `MethodName_StateUnderTest_ExpectedBehavior`
+    //These tests follow the naming convention `MethodName_StateUnderTest_ExpectedBehavior`
+    
+    //Demonstrates mocking
     @Test
     public void isJohnSmith_EmployeeIdMatchingJohnSmith_AssertReturnsTrue(){
         //Setup
         Long johnsId = 100L;
         Employee john = new Employee();
-        //Create `john` whose name is `John Smith` and matches our searched ID
+        //Create `john` whose name is `John Smith` and will match our searched ID
         john.setEmployeeId(johnsId);
         john.setFirstName("John");
         john.setLastName("Smith");
-        //Create an object for search to return, which holds our `john` employee
+        //Create an object for search to return, it holds our `john` employee
         Optional<Employee> searchResult;
         //Assign a non-null value for our result to hold
         searchResult = Optional.of(john); 
@@ -45,6 +47,28 @@ public class TestMyService{
         assertEquals(true, result);
     }
     
+    //Demonstrates mocking and uses matchers in its when() statement
+    @Test
+    public void isJohnSmith_NoEmployeeIdMatch_AssertReturnsFalse(){
+        //Setup
+        Long employeeId = 100L;
+        //Create an object to be returned by our search
+        Optional<Employee> searchResult;
+        searchResult = Optional.empty();
+        
+        //Mocks
+        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
+        when(employeeRepository.findByEmployeeId(anyLong())).thenReturn(searchResult);
+        
+        //Results
+        MyService myService = new MyService();
+        myService.setEmployeeRepository(employeeRepository);
+        boolean result = myService.isJohnSmith(employeeId);
+        
+        assertEquals(false, result);
+    }
+    
+    //Demonstrates testing a private method with reflection
     @Test
     public void addSecretly_Add2And5_AssertEquals7() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
         //Setup
@@ -53,7 +77,7 @@ public class TestMyService{
         Long expected = firstNum + secondNum;
         
         //Mocks
-        //Empty...
+        //Empty, no mocks required
         
         //Results
         MyService myService = new MyService();
